23c869cea99020df8563f724c12ec2061d5600a0
“Daniel <danielanderson.lopes0@gmail.com>
2024-07-23 16:19:38 -0300

    ✨ feat: Adiciona exceção para credenciais de login incorretas

---
+++ src/main/java/br/com/danllopes/usermanagment/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,5 @@
+package br.com.danllopes.usermanagment.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException{
+    public InvalidCredentialsException(String message) {super(message);}
+}
